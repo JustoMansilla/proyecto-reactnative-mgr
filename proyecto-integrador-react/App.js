b4afabede5a8dc4cc5b9b6071beff138add30fef
Justo Enrique Mansilla <jmansilla@udesa.edu.ar>
2021-06-16 19:03:03 -0300

    CSS para las tarjetas

--- proyecto-integrador-react/App.js
+++ proyecto-integrador-react/App.js
@@ -15,7 +15,11 @@ import { getData, getDataFetch } from './src/api/api';
 import 'react-native-gesture-handler';
 import {Screen_viewImportedCards} from './src/screens/Screen_viewImportedCards.js';
 import { Screen_import } from './src/screens/Screen_import';
-import { Screen_flatlist } from './src/screens/ Screen_flatlist';
+<<<<<<< HEAD
+import { Screen_flatlist } from './src/screens/Screen_flatlist';
+=======
+import { Screen_flatlist} from './src/screens/Screen_flatlist';
+>>>>>>> 867e46da5be184fe375487c5b3d3a6ebbba20c9d
 //import { NavigationContainer } from '@react-navigation/native';
 
 export default class App extends Component {
@@ -35,7 +39,7 @@ export default class App extends Component {
 
          <SafeAreaView style={{flex: 1, flexDirection: 'column'}}>
          <Text>Probando</Text>
-        <Screen_flatlist/>
+          <Screen_flatlist/>
          </SafeAreaView>
         
         // </NavigationContainer>    
--- proyecto-integrador-react/src/screens/Screen_flatlist.js
+++ proyecto-integrador-react/src/screens/Screen_flatlist.js
@@ -8,6 +8,7 @@ import {
   View,
   TouchableOpacity,
   FlatList,
+  StyleSheet
 } from 'react-native';
 import { getDataFetch } from '../api/api';
 
@@ -24,10 +25,17 @@ export class Screen_flatlist extends Component{
         keyExtractor = (item , idx) =>idx.toString();
         renderItem = ({item}) => {
         return (
+<<<<<<< HEAD:proyecto-integrador-react/src/screens/Screen_flatlist.js
+=======
             //traer todo lo que queramos de la api
-                    <Text>{item.name.first}</Text>,
-                    <Text>{item.name.last}</Text>
+                    <Text>{item.name.last}</Text>,
+                    <Text>{item.name.first}</Text>
+>>>>>>> 867e46da5be184fe375487c5b3d3a6ebbba20c9d:proyecto-integrador-react/src/screens/ screen_flatlist.js
 
+                <View style={styles.card}>
+                    <Text style={styles.text}>{item.name.first}</Text>
+                    <Text style={styles.text}>{item.name.last}</Text>
+                </View>
                 )
     }
 
@@ -44,7 +52,7 @@ export class Screen_flatlist extends Component{
     render(){
     
                 return(
-                            <View>
+                            <View >
                                     <FlatList data= {this.state.contactos} 
                                     renderItem= {this.renderItem} 
                                     keyExtractor = {this.keyExtractor}
@@ -57,3 +65,28 @@ export class Screen_flatlist extends Component{
 }
 
 // poner todo el css
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    card: {
+        backgroundColor: 'green',
+        borderRadius: 5,
+        borderStyle: 'solid',
+        borderWidth: 1,
+        margin: 5,
+        padding: 5,
+        width: 150,
+        height: 100
+    },
+    text: {
+        fontSize: 20
+    },
+    separator: {
+        borderBottomColor: 'black',
+        borderBottomWidth: 1
+    }
+})
