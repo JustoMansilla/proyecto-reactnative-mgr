d7f81c44a035e00cec8dcf01c8386384386b74d9
Lucas Grosz <50019297+LucasGrosz@users.noreply.github.com>
2021-06-22 16:13:19 -0300

    Modal casi terminado
    modal casi terminado

--- proyecto-integrador-react/src/components/Cards.js
+++ proyecto-integrador-react/src/components/Cards.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import { View, SafeAreaView ,ScrollView,TextInput, Image,Text,Pressable, Modal, Button} from 'react-native'
 import { styles } from '../styles/styles'
-// import ModalComponent from '../components/Modal'
+import Modal2 from '../components/Modal'
 import { getDataFetch } from '../api/api';
 
 
@@ -12,13 +12,24 @@ export class Cards extends Component {
     constructor(){
         super();
         this.state = {
-            // showModal: false,
+            showModal: false,
             
         }
     }
 
+    keyExtractor = (item , idx) =>idx.toString();
+    renderItem = ({item}) => {
+    return (
 
-    // showModal =  ()=> this.setState({isOpen: true});
+      
+        <Modal2
+        elemento = {item}/>
+     
+            
+            )
+}
+   
+    
 
 
       
@@ -27,24 +38,17 @@ export class Cards extends Component {
 
         return(
             <View style={styles.card}>
-            <Image style={styles.image} source={{uri: item.picture.thumbnail}}/>
+            <Image style={styles.image} source={{uri: this.props.elemento.picture.thumbnail}}/>
             
-            <Text style={styles.text}>Nombre completo: {item.name.first} {item.name.last}</Text> 
+            <Text style={styles.text}>Nombre completo: {this.props.elemento.name.first} {this.props.elemento.name.last}</Text> 
 
-            <Text style={styles.text}>E-mail: {item.email}</Text>
+            <Text style={styles.text}>E-mail: {this.props.elemento.email}</Text>
 
-            <Text style={styles.text}>Nacimiento: {item.dob.date.substring(0, 10)} ({item.dob.age})</Text>
-
-
-            <Button
-                title="Ver mas datos!"
-                color="white"
-                backgroundColor="black"
-                onPress={() => Alert.alert("Edad: " + item.dob.age, "Ciudad/Estado: " + item.location.city + "-" + item.location.state, "Pais: " + item.location.country, "Codigo Postal: " + item.location.postcode, "Fecha de Registro:: " + item.registered, "Codigo Postal: " + item.location.postcode, "Tel.: " + item.phone)}
-            />
+            <Text style={styles.text}>Nacimiento: {this.props.elemento.dob.date.substring(0, 10)} ({this.props.elemento.dob.age})</Text>
+            <Modal2/>
                 
         </View>
-
+    
       
         )
     };
@@ -52,3 +56,5 @@ export class Cards extends Component {
 }
 
 
+
+  
--- proyecto-integrador-react/src/screens/Screen_flatlist.js
+++ proyecto-integrador-react/src/screens/Screen_flatlist.js
@@ -38,26 +38,9 @@ export class Screen_flatlist extends Component{
 
           
             <Cards
-            item = {item}/>
+            elemento = {item}/>
          
-                // <View style={styles.card}>
-                //     <Image style={styles.image} source={{uri: item.picture.thumbnail}}/>
-                    
-                //     <Text style={styles.text}>Nombre completo: {item.name.first} {item.name.last}</Text> 
-
-                //     <Text style={styles.text}>E-mail: {item.email}</Text>
-
-                //     <Text style={styles.text}>Nacimiento: {item.dob.date.substring(0, 10)} ({item.dob.age})</Text>
-
-
-                //     <Button
-                //         title="Ver mas datos!"
-                //         color="white"
-                //         backgroundColor="black"
-                //         onPress={() => Alert.alert("Edad: " + item.dob.age, "Ciudad/Estado: " + item.location.city + "-" + item.location.state, "Pais: " + item.location.country, "Codigo Postal: " + item.location.postcode, "Fecha de Registro:: " + item.registered, "Codigo Postal: " + item.location.postcode, "Tel.: " + item.phone)}
-                //     />
-                        
-                // </View>
+                
                 )
     }
 
